8c967240bcaf6459e926a3ec330bccf020586391
Philipp Andresen <62700798+Phillemove@users.noreply.github.com>
2024-12-11 07:32:46 +0100

    Add files via upload

---
+++ ViewModel/EditUserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MVVMTest.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest.ViewModel
+{
+    public partial class EditUserViewModel : ObservableRecipient
+    {
+        [ObservableProperty]
+        private User user;
+
+        public EditUserViewModel()
+        {
+            User = new();
+        }
+
+        public EditUserViewModel(IList? obj)
+        {
+            User = new();
+
+            User? firstUser = obj?.OfType<User>().FirstOrDefault();
+
+            if (firstUser != null)
+            {
+                User.Name = firstUser.Name;
+                User.Email = firstUser.Email;
+            }
+        }
+    }
+}

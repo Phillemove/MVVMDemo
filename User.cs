065dfb00ba2b4d5611622341e992685b5ae85003
Philipp Andresen <62700798+Phillemove@users.noreply.github.com>
2024-12-11 07:27:03 +0100

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest.Model
+{
+    public partial class User : ObservableObject
+    {
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private string email;
+
+        public User()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+        }
+
+    }
+}
